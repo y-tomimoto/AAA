203596aeb2096577206db6d551873635f42bda3f
y-tomimoto <geektomimoto0104@gmail.com>
2020-04-26 19:59:40 +0900

    add sample activity

--- app/src/main/java/com/example/takitate/MainActivity.kt
+++ app/src/main/java/com/example/takitate/MainActivity.kt
@@ -2,11 +2,54 @@ package com.example.takitate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() { // 基本的に1つのアクティビティが１つの画面を表示している。これはAppCompatActivityからextendされている
+
+    override fun onCreate(savedInstanceState: Bundle?) { // アクティビティが生成されたときに実行される
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // ここでレイアウトを紐付ける（ビューやビューグループも設定できる）
+
+        val button = findViewById<Button>(R.id.calculate) // これはwidget別に管理されていて、別のアクティビティからも、caluculateにはアクセスできそう
+
+        // 下記はoncreate内で実行しないと、Expecting member declaration エラーが出る
+        button.setOnClickListener {
+            // ここに任意の処理を実装する
+            // 妥当かどうか
+            var isValid = true
+
+            val priceEditText = findViewById<EditText>(R.id.price)
+            val priceText = priceEditText.text.toString()
+
+            if (priceText.isEmpty()) {
+                // 定価が未入力
+                priceEditText.error = getString(R.string.price_error)
+                isValid = false
+            }
+
+            val discountEditText = findViewById<EditText>(R.id.discount)
+            val discountText = discountEditText.text.toString()
+
+            if (discountText.isEmpty()) {
+                // 割引率が未入力
+                discountEditText.error = getString(R.string.discount_error)
+                isValid = false
+            }
+
+            if (isValid) {
+                // 文字列を整数型に変換
+                val price = priceText.toInt()
+                val discount = discountText.toInt()
+
+                // ここで画面遷移を行う
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("price", prßice)
+            intent.putExtra("discount", discount)
+            startActivity(intent)
+            }
+        }
     }
 }
