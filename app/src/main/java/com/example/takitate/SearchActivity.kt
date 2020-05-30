5a5aeb24f7f58c4dc34e6006be9a496464e1aa2d
y-tomimoto <geektomimoto0104@gmail.com>
2020-05-31 01:56:10 +0900

    検索機能を追加

--- app/src/main/java/com/example/takitate/SearchActivity.kt
+++ app/src/main/java/com/example/takitate/SearchActivity.kt
@@ -1,12 +1,52 @@
 package com.example.takitate
 
-import androidx.appcompat.app.AppCompatActivity
+import android.database.Cursor
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SearchActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val disp = findViewById<TextView>(R.id.disp)
+        val searchBtn = findViewById<Button>(R.id.startSearch)
+        val searchText = findViewById<EditText>(R.id.searchText)
+
+        searchBtn.setOnClickListener {
+            // テキストを初期化している
+            disp.text = "SEARCH RESULTS:\n";
+            val str = searchText.text.toString()
+            val database = SampleDBOpenHelper(this).readableDatabase
+            val texts = mutableListOf<String>()
+            val c: Cursor = database.query(
+                "texts",
+                arrayOf("text"),
+                "text=?",
+                arrayOf(str),
+                null,
+                null,
+                null
+            )
+            c.use {
+                    // カーソルで順次処理していく
+                    while(c.moveToNext()) {
+                        // 保存されているテキストを得る
+                        val text = c.getString(c.getColumnIndex("text"))
+                        texts.add(text)
+                    }
+                }
+
+            database.close()
+            disp.text = texts.toString()
+        }
+
     }
+
+
 }
