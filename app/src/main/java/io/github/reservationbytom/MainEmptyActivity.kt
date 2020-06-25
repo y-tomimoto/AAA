60a0ec3b61026be2e89710aedad808a2b1ec8065
y-tomimoto <geektomimoto0104@gmail.com>
2020-06-25 22:48:39 +0900

    login済みかどうかでlogin動作を変更

--- app/src/main/java/io/github/reservationbytom/MainEmptyActivity.kt
+++ app/src/main/java/io/github/reservationbytom/MainEmptyActivity.kt
@@ -3,6 +3,7 @@ package io.github.reservationbytom
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
 
 
 class MainEmptyActivity : AppCompatActivity() {
@@ -10,10 +11,12 @@ class MainEmptyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         // go straight to main if a token is stored
-        val activityIntent: Intent = if (true) { // TODO 条件式にFacebook Tokenを追加
-            Intent(this, MainActivity::class.java)
-        } else {
+        // Reffer to https://stackoverflow.com/questions/29366945/facebook-accesstoken-getaccesstoken-is-null-on-opening-of-app-even-after-first-l
+        // Reffer to https://android.jlelse.eu/login-and-main-activity-flow-a52b930f8351
+        val activityIntent: Intent = if (AccessToken.getCurrentAccessToken() == null) {
             Intent(this, LoginActivity::class.java)
+        } else {
+            Intent(this, MainActivity::class.java)
         }
         startActivity(activityIntent)
         finish()
