142e45494176cc0f72662ad2531d7d41bc25c89b
富本悠介 <geektomimoto0104@gmail.com>
2020-11-13 19:48:26 +0900

    add location entity

--- app/src/main/java/io/github/reservationbytom/service/persistence/db/entity/LocationEntity.kt
+++ app/src/main/java/io/github/reservationbytom/service/persistence/db/entity/LocationEntity.kt
@@ -2,9 +2,11 @@ package io.github.reservationbytom.service.persistence.db.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class Location(
+  @PrimaryKey val uid: Int,
   @ColumnInfo(name = "longitude") val longitude: String?,
   @ColumnInfo(name = "latitude") val latitude: String?
 )
