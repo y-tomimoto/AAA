2d2dc20485e674e69ba4948ed4bfb8ef48ab510d
富本悠介 <geektomimoto0104@gmail.com>
2021-01-23 20:44:06 +0900

    feat: add add_review ui to navigation

--- app/src/main/java/com/company/takitate/ui/map/MapBottomSheetFragment.kt
+++ app/src/main/java/com/company/takitate/ui/map/MapBottomSheetFragment.kt
@@ -7,9 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.company.takitate.R
 import com.company.takitate.databinding.FragmentMapBottomSheetBinding
 import com.company.takitate.viewmodel.RecruitAPIResponseShopViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class MapBottomSheetFragment : Fragment() {
 
@@ -26,9 +28,19 @@ class MapBottomSheetFragment : Fragment() {
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
+
     binding = DataBindingUtil.inflate(inflater,R.layout.fragment_map_bottom_sheet, container,false) // bindingクラスを生成
     binding.vm = viewModel
     binding.lifecycleOwner = this // LiveDataが値の更新を検知するように設定する
+
+    val fab: View = binding.fab
+    fab.setOnClickListener { view ->
+      // ReviewFragmentへ以降する。このとき、店舗IDをFragmentに渡す。
+      println(viewModel.focusedShop.value?.id)
+      findNavController().navigate(R.id.action_Map_to_AddReview)
+    }
+
+
     return binding.root
   }
 }
