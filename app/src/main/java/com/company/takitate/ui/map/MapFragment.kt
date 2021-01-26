d543586f0e30978c0eea15052feb235bb52811e8
富本悠介 <geektomimoto0104@gmail.com>
2021-01-26 20:18:47 +0900

    style: add restaurant icon

--- app/src/main/java/com/company/takitate/ui/map/MapFragment.kt
+++ app/src/main/java/com/company/takitate/ui/map/MapFragment.kt
@@ -22,6 +22,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
