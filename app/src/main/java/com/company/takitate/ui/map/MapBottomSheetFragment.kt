2b57a8b0e45cf3c40c44a2f666920c726d49137a
富本悠介 <geektomimoto0104@gmail.com>
2021-02-02 23:36:03 +0900

    feat: modern gradation with bottom sheet background

--- app/src/main/java/com/company/takitate/ui/map/MapBottomSheetFragment.kt
+++ app/src/main/java/com/company/takitate/ui/map/MapBottomSheetFragment.kt
@@ -1,17 +1,19 @@
 package com.company.takitate.ui.map
 
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.DrawableContainer
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.company.takitate.R
 import com.company.takitate.databinding.FragmentMapBottomSheetBinding
 import com.company.takitate.viewmodel.RecruitAPIResponseShopViewModel
-import com.google.android.material.snackbar.Snackbar
+
 
 class MapBottomSheetFragment : Fragment() {
 
@@ -29,16 +31,25 @@ class MapBottomSheetFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View? {
 
-    binding = DataBindingUtil.inflate(inflater,R.layout.fragment_map_bottom_sheet, container,false) // bindingクラスを生成
-    binding.vm = viewModel
+    binding = DataBindingUtil.inflate(
+      inflater,
+      R.layout.fragment_map_bottom_sheet,
+      container,
+      false
+    ) // bindingクラスを生成
+    binding.viewModel = viewModel
     binding.lifecycleOwner = this // LiveDataが値の更新を検知するように設定する
 
     val fab: View = binding.fab
     fab.setOnClickListener { view ->
       // ReviewFragmentへ以降する。このとき、店舗IDをFragmentに渡す。
-      println(viewModel.focusedShop.value?.id)
       findNavController().navigate(R.id.action_Map_to_AddReview)
     }
+
+    val animationDrawable = binding.contentBox.background  as AnimationDrawable
+    animationDrawable.setEnterFadeDuration(2000)
+    animationDrawable.setExitFadeDuration(4000)
+    animationDrawable.start()
 
 
     return binding.root
