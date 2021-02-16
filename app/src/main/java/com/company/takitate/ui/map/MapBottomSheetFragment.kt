7d7d55b4dc2724a0ebc30fd0db447e44c4698dbc
富本悠介 <geektomimoto0104@gmail.com>
2021-02-16 13:53:55 +0900

    feat: bind image to bottom sheet

--- app/src/main/java/com/company/takitate/ui/map/MapBottomSheetFragment.kt
+++ app/src/main/java/com/company/takitate/ui/map/MapBottomSheetFragment.kt
@@ -8,10 +8,12 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.company.takitate.R
 import com.company.takitate.databinding.FragmentMapBottomSheetBinding
 import com.company.takitate.viewmodel.RecruitAPIResponseShopViewModel
+import com.squareup.picasso.Picasso
 
 
 class MapBottomSheetFragment : Fragment() {
@@ -20,6 +22,9 @@ class MapBottomSheetFragment : Fragment() {
 
   // プロパティデリゲートでviewModelを取得する方法
   private val viewModel:RecruitAPIResponseShopViewModel by activityViewModels()
+
+  // ForcusされたShopの画像をloadするために利用。
+  private val recruitAPIResponseFocusedShopViewModel: RecruitAPIResponseShopViewModel by activityViewModels()
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
@@ -47,5 +52,19 @@ class MapBottomSheetFragment : Fragment() {
 
 
     return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    // ここでViewModelから流れてきた値を受け取る.
+    recruitAPIResponseFocusedShopViewModel.focusedShop.observe(viewLifecycleOwner, Observer { shop ->
+      // 対象がupdateされたとき、画像をbindする。
+      Picasso.get()
+        .load(shop.logo_image)
+        .placeholder(R.drawable.ic_baseline_add_24)
+        .error(R.drawable.ic_baseline_add_24)
+        .into(binding.profileImage);
+    })
   }
 }
